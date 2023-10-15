871c6f3e5492a625185a3d90409254e8b587b92b
Emily Ng <emily.ng889@gmail.com>
2023-10-14 19:38:09 -0600

    removed flask files

--- fish_tableau_project_ELT_s3_dbt_prefect/run.py
+++
@@ -1,7 +0,0 @@
-from api import create_app
-from settings import DB_NAME, DB_USER, DB_PASSWORD
-
-app=create_app(DB_NAME, DB_USER, DB_PASSWORD)
-app.run(debug=True)
-
-
--- fish_tableau_project_ELT_s3_dbt_prefect/settings.py
+++ fish_tableau_project_ELT_s3_dbt_prefect/settings.py
@@ -2,14 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_NAME=os.getenv("DB_NAME")
-DB_USER=os.getenv("DB_USER")
-DB_PASSWORD=os.getenv("DB_PASSWORD")
-
-TEST_DB_NAME=os.getenv("TEST_DB_NAME")
-TEST_DB_USER=os.getenv("TEST_DB_USER")
-TEST_DB_PASSWORD=os.getenv("TEST_DB_PASSWORD")
-
 bucket_name=os.getenv("BUCKET_NAME")
 dbt_file_path=os.getenv("DBT_FILE_PATH")
 
